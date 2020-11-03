348f8a1964984091e6d587d4917bc91dc2d89e00
Fabrice Thilaw <fabricethilaw@users.noreply.github.com>
2020-11-03 13:45:53 +0100

    Upgraded dependencies - Fixed code duplicate

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,9 +5,18 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## # [0.0.3] - 2020-11-03
+
+### Fixed
+
+Upgraded dependencies 
+Fixed code duplicates
+
+
+
 ## # [0.0.2] - 2020-10-31
 
-# ### Changed
+### Changed
 
 ConnectivityResult class : documented and its fields renamed.  
 
--- core/src/main/java/com/fabricethilaw/sonarnet/internal/InternetReachability.kt
+++ core/src/main/java/com/fabricethilaw/sonarnet/internal/InternetReachability.kt
@@ -2,13 +2,11 @@ package com.fabricethilaw.sonarnet.internal
 
 
 import android.util.Log
+import com.fabricethilaw.sonarnet.InternetStatus
 import com.fabricethilaw.sonarnet.InternetStatus.*
 import com.fabricethilaw.sonarnet.InternetStatusCallback
 import com.fabricethilaw.sonarnet.internal.interfaces.InternetReachabilityInterface
-import okhttp3.Call
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
+import okhttp3.*
 import java.io.IOException
 import java.net.HttpURLConnection.HTTP_NO_CONTENT
 
@@ -26,41 +24,58 @@ internal class InternetReachability : InternetReachabilityInterface {
     private fun makeProbeRequest(url: String, callback: InternetStatusCallback) {
         val client = OkHttpClient()
 
-        val request: Request = Request.Builder()
-            .url(url)
-            .get()
-            .addHeader("content-type", "application/json")
-            .addHeader("cache-control", "no-cache")
-            .build()
+        val request: Request = buildRequest(url)
 
         // The Google connectivity check url
         // returns HTTP code 204
         val expectedHttpCode: Int = HTTP_NO_CONTENT
 
         try {
-            client.newCall(request).enqueue(object : okhttp3.Callback {
-                override fun onFailure(call: Call, e: IOException) {
-                    callback(NO_INTERNET)
-                    Log.e("INTERNET STATUS", "FAILURE, ${e.message}")
-                }
-
-                override fun onResponse(call: Call, response: Response) {
-                    val internetStatus =
-                        if (response.isSuccessful && response.code == expectedHttpCode) {
-                            INTERNET
-                        } else {
-                            CAPTIVE_PORTAL // connection might be intercepted
-                        }
-
-                    Log.e("INTERNET STATUS", "Http code: ${response.code}")
-                    callback(internetStatus)
-                }
-            })
+            client.newCall(request).enqueue(requestCallback(callback, expectedHttpCode))
         } catch (e: Exception) {
             // we just catch any exception that may occurs
         }
     }
 
+    private fun buildRequest(url: String): Request {
+        return Request.Builder()
+            .url(url)
+            .get()
+            .addHeader("content-type", "application/json")
+            .addHeader("cache-control", "no-cache")
+            .build()
+    }
+
+    private fun resolveProbeResult(
+        response: Response,
+        expectedHttpCode: Int
+    ): InternetStatus {
+        return if (response.isSuccessful && response.code == expectedHttpCode) {
+            INTERNET
+        } else {
+            CAPTIVE_PORTAL // connection might be intercepted
+        }
+    }
+
+    private fun requestCallback(
+        internetCallback: InternetStatusCallback,
+        expectedHttpCode: Int
+    ): Callback {
+        return object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                internetCallback(NO_INTERNET)
+                Log.e("INTERNET STATUS", "FAILURE, ${e.message}")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val internetStatus =
+                    resolveProbeResult(response, expectedHttpCode)
+
+                Log.e("INTERNET STATUS", "Http code: ${response.code}")
+                internetCallback(internetStatus)
+            }
+        }
+    }
 
     private val GOOGLE_CONNECTIVITY_CHECK_URL = "https://connectivitycheck.gstatic.com/generate_204"
 }

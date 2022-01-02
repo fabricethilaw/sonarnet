6e162790f2b9786d41ac99dd30235e68bd2090a7
thilawfabrice <fabricethilaw@gmail.co>
2022-01-02 15:42:33 +0100

    Unit tests

--- core/src/main/java/com/fabricethilaw/sonarnet/internal/InternetReachability.kt
+++ core/src/main/java/com/fabricethilaw/sonarnet/internal/InternetReachability.kt
@@ -7,12 +7,10 @@ import com.fabricethilaw.sonarnet.InternetStatus.*
 import com.fabricethilaw.sonarnet.InternetStatusCallback
 import com.fabricethilaw.sonarnet.internal.interfaces.InternetReachabilityInterface
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.jetbrains.annotations.TestOnly
 import java.net.HttpURLConnection
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
@@ -25,7 +23,7 @@ internal class InternetReachability : InternetReachabilityInterface {
 
     override fun ping(callback: InternetStatusCallback) {
         CoroutineScope(IO).launch {
-          callback.invoke( makeProbeRequest())
+            callback.invoke(makeProbeRequest())
         }
 
     }
@@ -36,7 +34,7 @@ internal class InternetReachability : InternetReachabilityInterface {
     }
 
 
-     suspend fun makeProbeRequest(): InternetStatus {
+    private suspend fun makeProbeRequest(): InternetStatus {
         val url = GOOGLE_CONNECTIVITY_CHECK_URL
         return withContext(IO) {
             try {
@@ -47,10 +45,10 @@ internal class InternetReachability : InternetReachabilityInterface {
                 httpConnection.connectTimeout = 2000
                 httpConnection.useCaches = false
                 httpConnection.setRequestProperty("Content-Type", "application/json; utf-8")
-                httpConnection.connect(); //connect
-                val responseLength: Int = httpConnection.contentLength;
+                httpConnection.connect()
+                val responseLength: Int = httpConnection.contentLength
                 val responseCode = httpConnection.responseCode //this is http(s) response code
-                Log.i("Sonarnet", "Response Code : $responseCode");
+                Log.i("Sonarnet", "Response Code : $responseCode")
                 httpConnection.disconnect()
                 resolveProbeResult(responseLength, responseCode)
             } catch (e: java.lang.Exception) {
@@ -60,23 +58,28 @@ internal class InternetReachability : InternetReachabilityInterface {
         }
     }
 
+    @TestOnly()
+    fun testProbeResult(responseContentLength: Int, httpCode: Int): InternetStatus {
+        return resolveProbeResult(responseContentLength, httpCode)
+    }
+
     private fun resolveProbeResult(
         responseLength: Int,
         httpCode: Int
     ): InternetStatus {
-        //GOOGLE_CONNECTIVITY_CHECK_URL is expected to return HTTP 204
+        // GOOGLE_CONNECTIVITY_CHECK_URL is expected to return HTTP 204
         // with no content as a response
         return if (httpCode == 204 && responseLength == 0
         ) {
             INTERNET
         } else {
-            //the request would have been intercepted
+            // the request would have been intercepted
             CAPTIVE_PORTAL
         }
     }
 
     companion object {
-        private const val GOOGLE_CONNECTIVITY_CHECK_URL =
+        const val GOOGLE_CONNECTIVITY_CHECK_URL =
             "https://connectivitycheck.gstatic.com/generate_204"
     }
 }
---
+++ core/src/test/java/com/fabricethilaw/sonarnet/internal/InternetReachabilityTest.kt
@@ -0,0 +1,86 @@
+package com.fabricethilaw.sonarnet.internal
+
+import com.fabricethilaw.sonarnet.InternetStatus
+import junit.framework.TestCase
+import org.junit.Test
+import kotlin.random.Random
+
+class InternetReachabilityTest : TestCase() {
+
+    @Test
+    fun connectivity_check_url_is_correct() {
+        assertEquals(
+            InternetReachability.GOOGLE_CONNECTIVITY_CHECK_URL,
+            "https://connectivitycheck.gstatic.com/generate_204"
+        )
+    }
+
+    @Test
+    fun test_probe_result_result_internet() {
+        val internetReachability = InternetReachability()
+        val responseLength = 0
+        val httpCode = 204
+
+        assertEquals(
+            internetReachability.testProbeResult(
+                responseLength,
+                httpCode
+            ),
+            InternetStatus.INTERNET
+        )
+    }
+
+    @Test
+    fun test_probe_result_wrong_http_code() {
+        val internetReachability = InternetReachability()
+        val responseLength = 0
+        val httpCode = Random.nextInt(100, 203)
+
+        assertEquals(
+            internetReachability.testProbeResult(
+                responseLength,
+                httpCode
+            ),
+            InternetStatus.CAPTIVE_PORTAL
+        )
+    }
+
+    @Test
+    fun test_probe_result_wrong_http_code_2() {
+        val internetReachability = InternetReachability()
+        val responseLength = 0
+        val httpCode = Random.nextInt(205, 599)
+
+        assertEquals(
+            internetReachability.testProbeResult(
+                responseLength,
+                httpCode
+            ),
+            InternetStatus.CAPTIVE_PORTAL
+        )
+    }
+
+    @Test
+    fun test_probe_result_wrong_http_response_length() {
+        val internetReachability = InternetReachability()
+        val responseLength = Random.nextInt(1, Int.MAX_VALUE)
+        val httpCode = 204
+
+        assertEquals(
+            internetReachability.testProbeResult(
+                responseLength,
+                httpCode
+            ),
+            InternetStatus.CAPTIVE_PORTAL
+        )
+    }
+
+
+    fun testPing() {}
+
+    fun testTestPing() {}
+
+    fun testMakeProbeRequest() {}
+
+
+}
